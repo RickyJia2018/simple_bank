9b7daccabed9528c0ab77237c3e40f5950bcb80a
NingqianJia <ricky.jia2018@gmail.com>
2023-07-12 00:59:23 -0700

    fix transfer deadlock

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -94,24 +94,41 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		})
 		// fmt.Println(txName, "get acc 1 for update")
 
-		result.FromAccount, err = q.AddAccountBalane(ctx, AddAccountBalaneParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
+		if arg.FromAccountID < arg.ToAccountID {
 
-		result.ToAccount, err = q.AddAccountBalane(ctx, AddAccountBalaneParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
-		if err != nil {
-			return err
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 		}
 
 		return nil
 	})
 
 	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalane(ctx, AddAccountBalaneParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAccountBalane(ctx, AddAccountBalaneParams{
+		ID:     account2.ID,
+		Amount: amount2,
+	})
+	if err != nil {
+		return
+	}
+	return account1, account2, err
 }
